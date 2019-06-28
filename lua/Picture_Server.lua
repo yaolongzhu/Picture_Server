306306dd40720adfa931f228d5d67774f8ae982e
yaolongzhu <yaolongzhu0612@gmail.com>
2019-06-28 15:56:47 +0800

    yaolong

--- lua/Picture_Server.lua
+++ lua/Picture_Server.lua
@@ -7,6 +7,20 @@
 --
 local Utils = require("Utils")
 
+local gm_path = ngx.var.gm_path
+local request_uri = ngx.var.request_uri;
+local file = ngx.var.file;
+local request_uri_split = splitStr(file, ",");
+
+local origial_full_path = string.sub(request_uri_split[1],0, string.len(request_uri_split[1]) - string.len("/resize"))
+local width = string.sub(request_uri_split[2], string.len("w_") +1, string.len(request_uri_split[2]))
+local length = string.sub(request_uri_split[3], string.len("h_") +1, string.len(request_uri_split[3]))
+
+Utils.printToNG(origial_full_path)
+Utils.printToNG(width)
+Utils.printToNG(length)
+
+
 ngx.say("<p>hello, world, in Picture Server</p>")
 ngx.say(ngx.var.uri)
-Utils.print(ngx.var.file)
+Utils.printToNG(ngx.var.file)
