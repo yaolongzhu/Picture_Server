ccc800e3d16937ca0c7af5b423b0725c092f808e
yaolongzhu <yaolongzhu0612@gmail.com>
2019-06-28 15:59:30 +0800

    yaolong

--- lua/Utils.lua
+++ lua/Utils.lua
@@ -8,12 +8,31 @@
 
 local M={};
 
-function print(str)
+function printToNG(str)
     ngx.say(str)
 end
 
+function splitStr(str, reps)
+    local r = {};
+    if (str == nil) then return nil; end
+    string.gsub(str, "[^"..reps.."]+", function(w) table.insert(r, w) end);
+    return r;
+end
 
 
-M.print = print
+--srcStr = "/thumbnails/aergegre/test.jpg/resize,w_200,h_300"
+--splitByStr = ","
+--splitResult = splitStr(srcStr, splitByStr);
+
+--print(splitResult[1])
+--print( string.sub(splitResult[1],0, string.len(splitResult[1]) - string.len("/resize")) )
+--print( string.sub(splitResult[2], string.len("w_") +1, string.len(splitResult[2])) )
+--print( string.sub(splitResult[3], string.len("h_") +1, string.len(splitResult[3])) )
+
+
+--print(string.sub(srcStr, 0, string.len(srcStr)-findLastIndex(srcStr, "/")))
+
+M.printToNG = printToNG
+M.splitStr = splitStr
 return M
 
